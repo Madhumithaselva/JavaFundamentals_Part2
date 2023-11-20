1d4a6ad15d648cb2034a34e2643f4ab1073ca726
Madhu <madhu8129@gmail.com>
2023-11-20 14:09:40 +0100

    Commited first program to print Current date

---
+++ src/main/java/se/lexicon/JavaTimeExercises1.java
@@ -0,0 +1,10 @@
+package se.lexicon;
+
+import java.time.LocalDate;
+
+public class JavaTimeExercises1 {
+    public static void main(String[] args) {
+        LocalDate today= LocalDate.now();
+        System.out.println("Local Date: "+today);
+    }
+}
