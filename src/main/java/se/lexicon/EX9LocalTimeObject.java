80a769ea52a117f5ebd3b74cee01ce14024febed
Madhu <madhu8129@gmail.com>
2023-11-21 14:48:58 +0100

    EX 9 created : TO create a local current timestamp and dispaly that

---
+++ src/main/java/se/lexicon/EX9LocalTimeObject.java
@@ -0,0 +1,11 @@
+package se.lexicon;
+
+import java.time.LocalTime;
+
+public class EX9LocalTimeObject {
+    public static void main(String[] args){
+        LocalTime currentTime=LocalTime.now();
+        System.out.println("Current Time now is "+currentTime);
+    }
+
+}
